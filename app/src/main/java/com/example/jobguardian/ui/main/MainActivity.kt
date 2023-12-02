8bb2f137a8cd392a94628adce557df191a5c0d67
Edward <edwardwoda05@gmail.com>
2023-12-02 19:51:15 +0700

    initial commit

--- app/src/main/java/com/example/jobguardian/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/jobguardian/ui/main/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.jobguardian.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.jobguardian.R
+import com.example.jobguardian.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity  : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.nav_view)
+        setSupportActionBar(toolbar)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/jobguardian/ui/main/ui/detection/DetectionViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.jobguardian.ui.main.ui.detection
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DetectionViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Detection Fragment"
+    }
+    val text: LiveData<String> = _text
+}
