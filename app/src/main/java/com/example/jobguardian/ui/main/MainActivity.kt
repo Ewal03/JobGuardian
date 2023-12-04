cb6d618f8b77d2becd30cb1be20a56a8c3574801
Edward <edwardwoda05@gmail.com>
2023-12-04 14:40:14 +0700

    initial commit

--- app/src/main/java/com/example/jobguardian/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/jobguardian/ui/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.jobguardian.ui.main
 
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,28 +9,28 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.jobguardian.R
 import com.example.jobguardian.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity  : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val toolbar: Toolbar = findViewById(R.id.nav_view)
-        setSupportActionBar(toolbar)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
-
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_detection, R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/jobguardian/ui/main/ui/detection/DetectionFragment.kt
+++ app/src/main/java/com/example/jobguardian/ui/main/ui/detection/DetectionFragment.kt
@@ -13,8 +13,6 @@ class DetectionFragment : Fragment() {
 
     private var _binding: FragmentDetectionBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +26,6 @@ class DetectionFragment : Fragment() {
         _binding = FragmentDetectionBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        detectionViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
